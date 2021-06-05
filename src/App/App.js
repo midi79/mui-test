dab7891423a6d0558feb081d39328551533e37d3
Anderson <andersonlee@Andersonui-MacBookPro.local>
2021-06-05 14:13:06 +0900

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import SideMenu from "../components/SideMenu";
+import { makeStyles, CssBaseline } from "@material-ui/core";
+import Header from "../components/Header";
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: "320px",
+    width: "100%",
+  },
+});
+
+function App() {
+  const classes = useStyles();
+  return (
+    <>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+      <CssBaseline />
+    </>
+  );
+}
+
+export default App;
